91d937aad18af278e19e7836448f9c1bd43c820d
kathyleboeuf <kleboeuf@townisp.com>
2018-08-30 12:48:54 -0400

    states and stateful done

--- src/components/game.js
+++ src/components/game.js
@@ -2,17 +2,29 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guesses: [22, 45, 59],
+            feedback: 'really cold',
+            count: 4,
+        };
+    }
+    render() {
+
+
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.feedback} />
+                <GuessCount count={this.state.count} />
+                <GuessList guesses={this.state.guesses} />
+            </div >
+        );
+    };
 }
 
